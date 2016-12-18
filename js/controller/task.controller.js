f8f7188b9d0d5023bcb95c9bc05d39687d99fbbd
sellers1894 <sellers1894@yandex.ru>
2016-12-18 16:50:47 +0300

    Строки задания
    Добавление/редактирование заголовка

--- js/controller/task.controller.js
+++ js/controller/task.controller.js
@@ -11,11 +11,32 @@
 		var vm = this;
 		vm.tasks = dataservice.getTasks();
 		vm.editTitle = editTitle;
+		vm.addItem = addItem;
+		vm.editItemTitle = editItemTitle;
 
 		function editTitle(id){
 			var name = prompt("Тест", vm.tasks.tasks[id].name);
 			if (name){
 				vm.tasks.tasks[id].name = name;
+				dataservice.update();
+			}
+		}
+
+		function addItem(id){
+			vm.tasks.tasks[id].items.push({
+				name: "item "+(vm.tasks.tasks[id].items.length+1),
+				comments: [],
+				checkList: []
+			});
+			dataservice.update();
+			console.log(vm.tasks);
+		}
+
+		function editItemTitle(parent_id, id){
+			console.log(parent_id);
+			var name = prompt("Тест", vm.tasks.tasks[parent_id].items[id].name);
+			if (name){
+				vm.tasks.tasks[parent_id].items[id].name = name;
 				dataservice.update();
 			}
 		}
